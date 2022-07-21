b8ea6835ec600ecf2194b8058912f2923bd90397
OguzhanUmutlu <oguzhan.umutlu123@gmail.com>
2022-07-21 23:05:56 +0300

    fixes

--- commands/slash/example-multi.struct.js
+++ commands/slash/example-multi.struct.js
@@ -1,8 +1,5 @@
 new SlashCommandBuilder()
     .addStringOption(opt => opt.setName("test").setDescription("optStr"))
-    .addStringOption(opt => opt.setName("test").setDescription("optStr"))
-    .addStringOption(opt => opt.setName("test").setDescription("optStr"))
-    .addStringOption(opt => opt.setName("test").setDescription("optStr"))
     .setDescription("example, " + guild.name)
     .setName("multi-example")
 // Docs: https://discord.js.org/#/docs/builders/main/class/SlashCommandBuilder
