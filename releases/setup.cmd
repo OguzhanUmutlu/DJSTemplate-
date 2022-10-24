d4b8e55854f4fc0844e14abeae7c836fec15b607
Oğuzhan <oguzhan.umutlu123@gmail.com>
2022-10-24 21:34:47 +0300

    test fix

--- releases/setup.cmd
+++ releases/setup.cmd
@@ -1,2 +1,1 @@
-@echo off
 title V14 Discord.JS Template && curl https://raw.githubusercontent.com/OguzhanUmutlu/DJSTemplate/main/releases/setup.js -o setup.js && npm init -y && npm install zip && node setup.js && npm install && node index.js
